63adc6c35175bcf3f42efa24b32524e66de1e096
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-02-28 17:35:43 +0800

    Added additional scripts

---
+++ stop_wabot.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+. $HOME/.bash_profile
+sleep 5
+exit
+cd $HOME/wajs
+if [ -e $HOME/wajs/.wwebjs_auth/session/Default/Preferences ]; then
+    cp $HOME/wajs/.wwebjs_auth/session/Default/Preferences $HOME/wajs/Preferences-`date +%s`
+    sed -E -i 's/("exited_cleanly":\s*)false/\1:true/' $HOME/wajs/.wwebjs_auth/session/Default/Preferences
+    sed -E -i 's/("exit_type":\s*)"Crashed"/\1"Normal"/' $HOME/wajs/.wwebjs_auth/session/Default/Preferences
+fi
+node wabot.js
+
