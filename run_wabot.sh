d4063a53c3f3270525705a01b82a8a86f409b721
K H Soh <hju37823@outlook.com>
2024-02-29 23:06:48 +0800

    Updated to support user-specified directory for cloned repo on server

--- run_wabot.sh
+++ run_wabot.sh
@@ -2,11 +2,12 @@
 
 . $HOME/.bash_profile
 sleep 5
-cd $HOME/wabot
-if [ -e $HOME/wabot/.wwebjs_auth/session/Default/Preferences ]; then
-    cp $HOME/wabot/.wwebjs_auth/session/Default/Preferences $HOME/wabot/Preferences-`date +%s`
-    sed -E -i 's/("exited_cleanly":\s*)false/\1:true/' $HOME/wabot/.wwebjs_auth/session/Default/Preferences
-    sed -E -i 's/("exit_type":\s*)"Crashed"/\1"Normal"/' $HOME/wabot/.wwebjs_auth/session/Default/Preferences
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+if [ -e $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences ]; then
+    cp $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences $SCRIPTPATH/Preferences-`date +%s`
+    sed -E -i 's/("exited_cleanly":\s*)false/\1:true/' $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences
+    sed -E -i 's/("exit_type":\s*)"Crashed"/\1"Normal"/' $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences
 fi
 node wabot.js
 
--- start_wabot.sh
+++ start_wabot.sh
@@ -2,14 +2,15 @@
 
 . $HOME/.bash_profile
 sleep 15
-cd $HOME/wabot
-if [ -e $HOME/wabot/.wwebjs_auth/session/Default/Preferences ]; then
-    cp $HOME/wabot/.wwebjs_auth/session/Default/Preferences $HOME/wabot/Preferences-`date +%s`
-    sed -E -i 's/("exited_cleanly":\s*)false/\1:true/' $HOME/wabot/.wwebjs_auth/session/Default/Preferences
-    sed -E -i 's/("exit_type":\s*)"Crashed"/\1"Normal"/' $HOME/wabot/.wwebjs_auth/session/Default/Preferences
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+if [ -e $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences ]; then
+    cp $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences $SCRIPTPATH/Preferences-`date +%s`
+    sed -E -i 's/("exited_cleanly":\s*)false/\1:true/' $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences
+    sed -E -i 's/("exit_type":\s*)"Crashed"/\1"Normal"/' $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences
 fi
 
-FNLOG=$HOME/wabot/wabot.log
+FNLOG=$SCRIPTPATH/wabot.log
 FNSIZE=$(stat -c%s "$FNLOG")
 MAXSIZE=5000000
 
