e14e6120133738cad4bb2757e5b2c02d06c9b19e
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-03-01 12:09:47 +0800

    Updates to support user-specified server name

--- setup_before_wabot.sh
+++ setup_before_wabot.sh
@@ -19,6 +19,6 @@ echo export NODE_PATH=\"/usr/local/lib/node_modules\" >> ~/.bash_profile
 
 cat << __end
 Install the ssh public key of source PC/Mac to ~/.ssh/authorized_keys
-scp <SSH public key of source PC/Mac> $BOTNAME@zbwajsbotNN:~/srcpubkey
-ssh $BOTNAME@zbwajsbotNN "cat ~/srcpubkey >> ~/.ssh/authorized_keys"
+scp <SSH public key of source PC/Mac> $BOTNAME@$HOSTNAME:~/srcpubkey
+ssh $BOTNAME@$HOSTNAME "cat ~/srcpubkey >> ~/.ssh/authorized_keys"
 __end
