fc3b090641d7d8faaedb47e0e3d7e79fc4cfdb9b
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-05-30 16:17:53 +0800

    Fixed handling of group notification

--- wabot.js
+++ wabot.js
@@ -281,6 +281,37 @@ client.on('message_reaction', async (reaction) => {
 });
 
 
+client.on('group_join', async (notification) => {
+    // User has joined or been added to the group.
+    dtcon.log('Event: group_join', notification);
+    if (BOTINFO.STATE != BOT_ACTIVE) {
+        // BOT is not processing commands - get out
+        return;
+    }
+    let chat = await client.getChatById(notification.chatId);
+    let participant_name = "Unknown";
+    try {
+        let contact = await client.getContactById(notification.id.participant);
+        participant_name = contact.name;
+    }
+    catch (e) {
+        participant_name = "Unknown with error: " + JSON.stringify(e);
+    }
+    let grpjoininfo = {
+        group_id: notification.chatId,
+        group_name: chat.name,
+        participant_name: participant_name
+    };
+    let number = notification.id.participant.replace(/@.*$/, '');
+    await cmd_to_host(number, grpjoininfo, [], "group_join");
+});
+
+client.on('group_leave', (notification) => {
+    // User has left or been kicked from the group.
+    dtcon.log('Event: group_leave', notification);
+    //notification.reply('User left.');
+});
+
 client.on('group_update', async (notification) => {
     // Group information has been updated
     dtcon.log('Event: group_update', notification);
@@ -293,45 +324,8 @@ client.on('group_update', async (notification) => {
         group_id: notification.chatId,
         group_name: chat.name,
     };
-    let contact = await notification.getContact();
-    let number = contact.number.replace(/@.*$/, '');
+    let number = notification.id.participant.replace(/@.*$/, '');
     await cmd_to_host(number, grpupdateinfo, [], "group_update");
-});
-
-client.on('group_join', async (notification) => {
-    // User has joined or been added to the group.
-    dtcon.log('Event: group_join', notification);
-    if (BOTINFO.STATE != BOT_ACTIVE) {
-        // BOT is not processing commands - get out
-        return;
-    }
-    let chat = await client.getChatById(notification.chatId);
-    let participant_name = "Unknown";
-    try {
-        let contact = await notification.getContact();
-        participant_name = contact.pushname;
-    }
-    catch (e) {
-        participant_name = "Unknown with error: " + JSON.stringify(e);
-    }
-    let grpjoininfo = {
-        group_id: notification.chatId,
-        group_name: chat.name,
-        participant_name: participant_name
-    };
-    let number = contact.number.replace(/@.*$/, '');
-    await cmd_to_host(number, grpjoininfo, [], "group_join");
-});
-
-client.on('group_leave', (notification) => {
-    // User has left or been kicked from the group.
-    dtcon.log('Event: group_leave', notification);
-    //notification.reply('User left.');
-});
-
-client.on('group_update', (notification) => {
-    // Group picture, subject or description has been updated.
-    dtcon.log('Event: group_update', notification);
 });
 
 client.on('change_state', async (state) => {
