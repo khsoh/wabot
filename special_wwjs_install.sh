dbae01838d1645dc144ae1e9a4fa73e8d8af3064
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-06-11 09:44:34 +0800

    Added script to install special version of whatsapp-web.js

---
+++ special_wwjs_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+SCRIPTPATH="$( cd -- "$(dirname "$BASH_SOURCE[0]")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
+node install github:pedroslopez/whatsapp-web.js#webpack-exodus
+
