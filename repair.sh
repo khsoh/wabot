a8828494072b93501ae382c4f2e44abeeb2a6050
K H Soh <hju37823@outlook.com>
2024-09-11 09:09:17 +0800

    Added script to initiate re-pairing of whatsapp account

---
+++ repair.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Remove .wwebjs`_* subdirectories to initiate repairing
+sleep 15
+SCRIPTPATH="$( cd -- "$(dirname "$BASH_SOURCE[0]")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
+rm -rf .wwebjs_cache
+rm -rf .wwebjs_auth
+
+sudo reboot
+
