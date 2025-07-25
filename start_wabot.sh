4eb6849f77f404a46b07a0fcb0a999e5633b163a
K H Soh <hju37823@outlook.com>
2025-07-25 09:45:23 +0800

    Removed changing of Chrome Preferences file in start_wabot.sh

--- start_wabot.sh
+++ start_wabot.sh
@@ -5,8 +5,8 @@ SCRIPTPATH="$( cd -- "$(dirname "$BASH_SOURCE[0]")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 if [ -e $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences ]; then
     cp $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences $SCRIPTPATH/Preferences-`date +%s`
-    sed -E -i 's/("exited_cleanly":\s*)false/\1:true/' $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences
-    sed -E -i 's/("exit_type":\s*)"Crashed"/\1"Normal"/' $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences
+    # sed -E -i 's/("exited_cleanly":\s*)false/\1:true/' $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences
+    # sed -E -i 's/("exit_type":\s*)"Crashed"/\1"Normal"/' $SCRIPTPATH/.wwebjs_auth/session/Default/Preferences
 fi
 
 # Remove Preferences-* except for the latest 5 files
