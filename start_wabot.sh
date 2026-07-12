6ece6fb0c777a1492b8d02d850aaebcd39b5bdd8
K H Soh <hju37823@outlook.com>
2026-07-12 22:11:54 +0800

    Updated to allow for stopping of restart from reboot

--- start_wabot.sh
+++ start_wabot.sh
@@ -1,28 +1,34 @@
 #!/usr/bin/env bash
 
 sleep 15
-SCRIPTPATH="$( cd -- "$(dirname "$BASH_SOURCE[0]")" >/dev/null 2>&1 ; pwd -P )"
-cd $SCRIPTPATH
-if [ -e $SCRIPTPATH/session/Default/Preferences ]; then
-    cp $SCRIPTPATH/session/Default/Preferences $SCRIPTPATH/Preferences-`date +%s`
+SCRIPTPATH="$(
+    cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 || exit
+    pwd -P
+)"
+if [[ "$#" -gt 0 && -f "$SCRIPTPATH/donotstart" ]]; then
+    exit
+fi
+
+cd "$SCRIPTPATH" || exit
+if [ -e "$SCRIPTPATH/session/Default/Preferences" ]; then
+    cp "$SCRIPTPATH/session/Default/Preferences" "$SCRIPTPATH/Preferences-$(date +%s)"
     # sed -E -i 's/("exited_cleanly":\s*)false/\1:true/' $SCRIPTPATH/session/Default/Preferences
     # sed -E -i 's/("exit_type":\s*)"Crashed"/\1"Normal"/' $SCRIPTPATH/session/Default/Preferences
 fi
 
 # Remove Preferences-* except for the latest 5 files
-if ls Preferences-* 1> /dev/null 2>&1; then
-    OLDPREFS=$(ls Preferences-*|head --lines=-4)
-    [ -z "$OLDPREFS" ] || rm $OLDPREFS
+if ls Preferences-* 1>/dev/null 2>&1; then
+    OLDPREFS=$(ls Preferences-* | head --lines=-4)
+    [ -z "$OLDPREFS" ] || rm "$OLDPREFS"
 fi
 
 FNLOG=$SCRIPTPATH/wabot.log
 FNSIZE=$(stat -c%s "$FNLOG")
 MAXSIZE=5000000
 
-if (( FNSIZE > MAXSIZE )); then
-    rm $FNLOG
+if ((FNSIZE > MAXSIZE)); then
+    rm "$FNLOG"
 fi
 
-node wabot.js >> $FNLOG 2>&1
+node wabot.js >>"$FNLOG" 2>&1
 #node wabot.js
-
