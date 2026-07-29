64bfe2f28d4bda4c91dd080295c2f48b0f712370
K H Soh <hju37823@outlook.com>
2026-07-29 17:12:48 +0800

    Cleaned up start_wabot.sh to not save Chrome session preferences

--- start_wabot.sh
+++ start_wabot.sh
@@ -10,17 +10,6 @@ if [[ "$#" -eq 0 && -f "$SCRIPTPATH/donotstart" ]]; then
 fi
 
 cd "$SCRIPTPATH" || exit
-if [ -e "$SCRIPTPATH/session/Default/Preferences" ]; then
-    cp "$SCRIPTPATH/session/Default/Preferences" "$SCRIPTPATH/Preferences-$(date +%s)"
-    # sed -E -i 's/("exited_cleanly":\s*)false/\1:true/' $SCRIPTPATH/session/Default/Preferences
-    # sed -E -i 's/("exit_type":\s*)"Crashed"/\1"Normal"/' $SCRIPTPATH/session/Default/Preferences
-fi
-
-# Remove Preferences-* except for the latest 5 files
-if ls Preferences-* 1>/dev/null 2>&1; then
-    OLDPREFS=$(ls Preferences-* | head --lines=-4)
-    [ -z "$OLDPREFS" ] || rm "$OLDPREFS"
-fi
 
 FNLOG=$SCRIPTPATH/wabot.log
 FNSIZE=$(stat -c%s "$FNLOG")
@@ -31,4 +20,3 @@ if ((FNSIZE > MAXSIZE)); then
 fi
 
 node wabot.js >>"$FNLOG" 2>&1
-#node wabot.js
